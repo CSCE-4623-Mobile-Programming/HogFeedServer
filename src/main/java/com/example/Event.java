9ad319c08afe9934d105ad22b5f77a4f331657bd
Alejandro Urquieta Salazar <ales.urquieta@gmail.com>
2019-11-16 19:09:42 -0600

    Added @column definition for event entity

--- src/main/java/com/example/Event.java
+++ src/main/java/com/example/Event.java
@@ -10,7 +10,7 @@ public class Event
 {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
   @Column (name = "title")
