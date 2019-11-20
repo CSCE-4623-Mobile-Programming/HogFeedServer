456b367e5a4611362ec4a1fe15a4ec00e024e398
Alejandro Urquieta Salazar <ales.urquieta@gmail.com>
2019-11-20 17:24:37 -0600

    Defined length for variables

--- src/main/java/com/example/Event.java
+++ src/main/java/com/example/Event.java
@@ -5,29 +5,29 @@ import org.hibernate.validator.constraints.NotEmpty;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "event")
+@Table(name = "events")
 public class Event
 {
 
   @Id
-  //@TableGenerator(name = "EVENT", table = )
+  @TableGenerator(name = "EVENTS")
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
-  @Column (name = "title")
+  @Column (name = "title", length = 65536)
   @NotEmpty
   private String title;
 
-  @Column (name = "latitude")
+  @Column (name = "latitude", length = 65536)
   private String latitude;
 
-  @Column (name = "longitude")
+  @Column (name = "longitude", length = 65536)
   private String longitude;
 
-  @Column (name = "location")
+  @Column (name = "location", length = 65536)
   private String location;
 
-  @Column (name = "quantity")
+  @Column (name = "quantity", length = 65536)
   private int quantity;
 
 
