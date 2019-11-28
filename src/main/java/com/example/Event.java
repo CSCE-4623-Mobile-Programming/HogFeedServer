5c355ef43a5696a1076d0515dc1a83b74943111e
Alejandro Urquieta Salazar <ales.urquieta@gmail.com>
2019-11-28 02:07:02 -0600

    Tester Table

--- src/main/java/com/example/Event.java
+++ src/main/java/com/example/Event.java
@@ -10,7 +10,7 @@ public class Event
 
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
-  //@TableGenerator(name = "events")
+  @TableGenerator(name = "events")
   private long id;
 
   @Column (name = "title", length = 65536)
